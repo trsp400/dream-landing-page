ba4e8fab7316175fc2c1cd44c9f45e119342ab07
Marcos Freitas <marcos.freitas@e-capital-sa.com.br>
2021-02-26 17:03:28 -0300

    MDSVII-4 #comment Add Styled

--- src/components/CustomComponents/CheckBox/index.js
+++ src/components/CustomComponents/CheckBox/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import _ from 'lodash';
 
-import { FormStyled, CheckStyled, LabelStyled } from './styles';
+import {
+  FormStyled,
+  CheckStyled,
+  LabelStyled,
+  SpanInput,
+  SpanControl,
+  RadioLabel,
+} from './styles';
 
 const CheckBox = ({ options, inputValue, setInputValue }) => {
+  const chunkedData = _.chunk(options, 2);
+
   const handleInputChange = (e, option) => {
     if (inputValue.includes(option)) {
       return setInputValue(inputValue.filter(value => value !== option));
@@ -11,17 +21,38 @@ const CheckBox = ({ options, inputValue, setInputValue }) => {
   };
 
   return (
-    <FormStyled>
-      {options.map(option => (
-        <LabelStyled>
-          <CheckStyled
-            name={option}
-            type="checkbox"
-            checked={inputValue.find(inputV => inputV === option)}
-            onChange={e => handleInputChange(e, option)}
-          />
-          {option}
-        </LabelStyled>
+    <FormStyled className="d-flex col-12">
+      {chunkedData.map((row, rowIndex) => (
+        <div key={rowIndex} className="row col-6">
+          {row.map(col => (
+            <LabelStyled>
+              <SpanInput>
+                <CheckStyled
+                  name={col}
+                  type="checkbox"
+                  checked={inputValue.find(inputV => inputV === col)}
+                  onChange={e => handleInputChange(e, col)}
+                />
+                <SpanControl className="checkbox__control">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                    focusable="false"
+                  >
+                    <path
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="3"
+                      d="M1.73 12.91l6.37 6.37L22.79 4.59"
+                    />
+                  </svg>
+                </SpanControl>
+              </SpanInput>
+              <RadioLabel className="radio__label">{col}</RadioLabel>
+            </LabelStyled>
+          ))}
+        </div>
       ))}
     </FormStyled>
   );
--- src/components/CustomComponents/CheckBox/styles.js
+++ src/components/CustomComponents/CheckBox/styles.js
@@ -1,7 +1,90 @@
 import styled from 'styled-components';
 
-export const CheckStyled = styled.input``;
+export const CheckStyled = styled.input`
+  opacity: 0;
+  width: 1.5em;
+  height: 1.5em;
 
-export const LabelStyled = styled.label``;
+  &:focus + .checkbox__control {
+    box-shadow: 0 0 0 0.01em #fff,
+      0 0 0.15em 0.1em ${({ theme }) => theme.mobileTheme.colors.primary.orange};
+  }
 
-export const FormStyled = styled.div``;
+  &:checked + .checkbox__control svg {
+    transform: scale(1);
+  }
+
+  &:disabled + .checkbox__control {
+    color: #959495;
+  }
+`;
+
+export const LabelStyled = styled.label`
+  display: grid;
+  grid-template-columns: min-content auto;
+  grid-gap: 0.5em;
+  font: normal normal 400 1.3rem Trasandina;
+  margin-bottom: 3rem;
+`;
+
+export const SpanInput = styled.span`
+  display: grid;
+  grid-template-areas: 'checkbox';
+  justify-content: center;
+  align-content: center;
+
+  > * {
+    grid-area: checkbox;
+  }
+
+  input {
+    opacity: 0;
+    width: 1em;
+    height: 1em;
+
+    &:focus + .checkbox__control {
+      box-shadow: 0 0 0 0.05em
+          ${({ theme }) => theme.mobileTheme.colors.primary.blue},
+        0 0 0.15em 0.1em
+          ${({ theme }) => theme.mobileTheme.colors.primary.orange};
+    }
+
+    &:checked + .checkbox__control svg {
+      transform: scale(1);
+    }
+
+    &:disabled + .checkbox__control {
+      color: #959495;
+    }
+  }
+`;
+
+export const SpanControl = styled.span`
+  display: inline-grid;
+  width: 1.5em;
+  height: 1.5em;
+  border-radius: 0.25em;
+  border: 0.15em solid ${({ theme }) => theme.mobileTheme.colors.primary.orange};
+  color: ${({ theme }) => theme.mobileTheme.colors.primary.orange};
+
+  svg {
+    transition: transform 0.1s ease-in 25ms;
+    transform: scale(0);
+    transform-origin: bottom left;
+  }
+`;
+
+export const FormStyled = styled.div`
+  display: grid;
+  place-content: center;
+  grid-gap: 2rem;
+  padding: 2rem;
+  line-height: 1;
+`;
+
+export const RadioLabel = styled.span`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  color: #fff;
+`;
