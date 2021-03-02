380dff221dd5b5d9a511e1df080ccd88f4e23ef4
Marcos Freitas <marcos.freitas@e-capital-sa.com.br>
2021-03-02 18:01:18 -0300

    MDSVII-6 #comment Add Message Feedback

---
+++ src/components/CustomComponents/MessageFeedback/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TextStyled = styled.p`
+  font: normal normal var(--strong) 22px Trasandina;
+  color: #fff;
+  padding: 5px 0;
+  margin: 5px 0;
+`;
